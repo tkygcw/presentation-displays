a501c799d84dad9b7e3a8afe0acb5ae5106f176e
optimypos <optimypos@gmail.com>
2025-09-09 10:33:47 +0800

    - Add window overlay

--- android/src/main/kotlin/com/namit/presentation_displays/PresentationDisplay.kt
+++ android/src/main/kotlin/com/namit/presentation_displays/PresentationDisplay.kt
@@ -2,10 +2,12 @@ package com.namit.presentation_displays
 
 import android.app.Presentation
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Display
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.FrameLayout
 import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngineCache
@@ -24,6 +26,18 @@ class PresentationDisplay(context: Context, private val tag: String, display: Di
         flContainer.layoutParams = params
 
         setContentView(flContainer)
+
+        //此功能作用于主屏Activity返回桌面后，副屏View仍然显示
+        //This function is used to display the secondary
+        // screen View after the main screen activity returns to the desktop
+        if (Build.VERSION.SDK_INT >= 32) {
+        } else if (Build.VERSION.SDK_INT >= 26) {
+            // 画中画等详细请查看android sdk    For details such as picture in picture, please check the android sdk
+            window!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY)
+        } else {
+            // 8.0 以下的安卓版本要实现上述功能使用以下api  Android versions below 8.0 use the following apis to achieve the above functions
+            window!!.setType(WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY)
+        }
 
         val flutterView = FlutterView(context)
         flContainer.addView(flutterView, params)
